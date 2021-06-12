dc2fc02e33d3bf62e73ca406b8d8fe50cb13b673
Hristijan Nikoloski <h.nikoloski@outlook.com>
2021-06-12 23:09:26 +0200

    Game history text added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
@@ -12,8 +13,6 @@ const App = () => {
   const [currentMove, setCurrentMove] = useState(0);
 
   const current = history[currentMove];
-
-  console.log('History:', history);
 
   const winner = calculateWinner(current.board);
 
@@ -41,11 +40,17 @@ const App = () => {
 
     setCurrentMove(prev => prev + 1);
   };
+
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
